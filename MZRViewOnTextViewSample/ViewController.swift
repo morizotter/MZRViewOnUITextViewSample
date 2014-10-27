6a0d2bca64f7adf79c335552fcb88b5f3e5b3ba5
Naoki Morita <namorit@gmail.com>
2014-10-28 03:10:07 +0900

    First commit

--- MZRViewOnTextViewSample/ViewController.swift
+++ MZRViewOnTextViewSample/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.textView.textContainerInset = UIEdgeInsetsMake(100.0, 0.0, 0.0, 0.0)
+        let view = UIView(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(self.textView.frame), 100.0))
+        view.backgroundColor = UIColor.greenColor()
+        self.textView.addSubview(view)
+        
+        let button = UIButton.buttonWithType(.Custom) as UIButton
+        button.frame = CGRectMake(10.0, 10.0, 100.0, 44.0)
+        button.backgroundColor = UIColor.grayColor()
+        button.setTitle("Push Me!", forState: .Normal)
+        button.addTarget(self, action: "push:", forControlEvents: .TouchUpInside)
+        view.addSubview(button)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let notifications = [UIKeyboardWillShowNotification, UIKeyboardWillChangeFrameNotification, UIKeyboardWillHideNotification];
+        for notification in notifications {
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "fitKeyboard:", name: notification, object: nil)
+        }
     }
-
-
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func fitKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo
+        let duration = (userInfo?[UIKeyboardAnimationDurationUserInfoKey] as NSNumber).doubleValue
+        let curve = (userInfo?[UIKeyboardAnimationCurveUserInfoKey] as NSNumber).unsignedIntegerValue
+        let keyboardFrame = (userInfo?[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        let keyboardHeight = CGRectGetHeight(self.view.frame) - CGRectGetMinY(keyboardFrame)
+        
+        self.textViewBottomConstraint.constant = keyboardHeight
+        let options = UIViewAnimationOptions(UInt(curve))
+        UIView.animateWithDuration(duration, delay: 0.0, options: options, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func push(sender: UIButton) {
+        println("button pushed!")
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,11 @@
+# MZRViewOnUITextViewSample
+
+It's just a sample for explaning the combination of a view and UITextView.
+
+## Author
+
+- [Naoki Morita](http://moritanaoki.org)
+
+## LICENSE
+
+Feel free to use it!
