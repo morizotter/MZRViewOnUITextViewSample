aeae4dd51f7c41487448f4074ba691e2fcf5da35
Naoki Morita <namorit@gmail.com>
2014-10-28 01:46:06 +0900

    Initial Commit

---
+++ MZRViewOnTextViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MZRViewOnTextViewSample
+//
+//  Created by MORITA NAOKI on 2014/10/28.
+//  Copyright (c) 2014年 molabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
